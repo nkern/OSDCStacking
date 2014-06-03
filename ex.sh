39e6f62c02468db77673a541803b83f6f973d9bc
nkern <nkern@umich.edu>
2014-06-03 16:22:31 -0500

    deleted:    ex.py 	modified:   ex.sh

--- ex.sh
+++ ex.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+#PBS -l nodes=1:ppn=1,pmem=10mb,walltime=1:00
+#PBS -V
+#PBS -o /glusterfs/users/caustics1/nkern/OSDCStacking/
+#PBS -j oe
+#
 
-echo $UID
-sudo -s
-echo $UID
-
+cd /glusterfs/users/caustics1/nkern/OSDCStacking/
+echo $SHELL
+python ex.py
 
