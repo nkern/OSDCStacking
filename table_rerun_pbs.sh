3c0caf7343b2f782154d6929982f279aea666a7a
Nick Kern <nkern@umich.edu>
2014-08-26 00:10:38 -0500

    modified:   table_rerun_pbs.sh

--- table_rerun_pbs.sh
+++ table_rerun_pbs.sh
@@ -3,12 +3,13 @@
 #PBS -N @@job_name@@
 #PBS -l nodes=1:ppn=1,pmem=2000mb,walltime=2:00:00
 #PBS -V
-#PBS -o /glusterfs/users/caustics1/nkern/OSDCStacking/@@data_loc@@/@@write_loc@@/
+#PBS -o @@root@@/OSDCStacking/@@data_loc@@/@@write_loc@@/
 #PBS -j oe
 #
 
-cd /glusterfs/users/caustics1/nkern/OSDCStacking/@@data_loc@@/@@write_loc@@
+# Go To Working Directory
+cd @@root@@/OSDCStacking/@@data_loc@@/@@write_loc@@
 
-python /glusterfs/users/caustics1/nkern/OSDCStacking/@@filename@@.py @@run_num@@ 
-
+# Run Code
+python @@root@@/OSDCStacking/@@filename@@.py @@run_num@@ 
 
