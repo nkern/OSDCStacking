1fc824dc4f39c800de29ab17c1794ca71919be87
Ubuntu <ubuntu@torque-headnode-55880058-08-14-14.novalocal>
2015-01-24 14:30:26 -0600

    new file:   git_add.sh

---
+++ git_add.sh
@@ -0,0 +1,28 @@
+git add biglosclusters.csv
+git add bootstrap_rerun.sh
+git add bootstrap_run.sh
+git add caustic_params_pbs.py
+git add caustic_params.py
+git add centralsClusters2.csv
+git add get_syze.sh
+git add millennium_bootstrap.py
+git add millennium_cent_offset.py
+git add millennium_massmix.py
+git add millennium_recover.py
+git add millennium_stack.py
+git add new_halo_coords.csv
+git add richness_est.py
+git add stack_class.py
+git add table_analysis.py
+git add table_analysis.sh
+git add table_rerun_pbs.sh
+git add table_rerun.sh
+git add table_run_pbs.sh
+git add table_run.sh
+git add git_add.sh
+
+
+
+
+
+
